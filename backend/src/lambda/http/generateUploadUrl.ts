e52594fe919d620a6a47ba27ec6d76ac567eadce
Simon Otter <simonotter@gmail.com>
2020-12-04 17:58:52 +0000

    Added GenerateUploadUrl endpoint

--- backend/src/lambda/http/generateUploadUrl.ts
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -1,10 +1,42 @@
-// import 'source-map-support/register'
+import 'source-map-support/register';
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import * as AWS  from 'aws-sdk';
+import * as middy from 'middy';
+import { cors } from 'middy/middlewares';
 
-// import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+const s3 = new AWS.S3({
+  signatureVersion: 'v4'
+});
 
-// export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-//   const todoId = event.pathParameters.todoId
+const bucketName = process.env.IMAGES_S3_BUCKET;
+const urlExpiration: Number = Number(process.env.SIGNED_URL_EXPIRATION);
 
-//   // TODO: Return a presigned URL to upload a file for a TODO item with the provided id
-//   return undefined
-// }
+
+export const handler = middy(async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  const todoId = event.pathParameters.todoId;
+
+  const url = getUploadUrl(todoId);
+
+  // Return a presigned URL to upload a file for a TODO item with the provided id
+  return {
+    statusCode: 201,
+    body: JSON.stringify({
+      uploadUrl: url
+    })
+  };
+
+});
+
+function getUploadUrl(imageId: string) {
+  return s3.getSignedUrl('putObject', {
+    Bucket: bucketName,
+    Key: imageId,
+    Expires: urlExpiration
+  });
+}
+
+handler.use(
+  cors({
+    credentials: true
+  })
+);
